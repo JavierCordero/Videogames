9811a038b86dbf7714d337f0bf57dbec08210385
Javier Cordero <javiercorderocalvo@gmail.com>
2019-10-09 13:12:37 +0200

    Cambiado footer

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -3,7 +3,7 @@ document.write(
   "<li><a href='https://twitter.com/Ruiderillo' target = 'blank' class='icon style2 fa-twitter'><span class='label'>Twitter</span></a></li>",
   "<li><a href='https://www.facebook.com/javier.corderocalvo' target = 'blank' class='icon style2 fa-facebook'><span class='label'>Facebook</span></a></li>",
   "<li><a href='https://www.instagram.com/ruiderillo/' target = 'blank' class='icon style2 fa-instagram'><span class='label'>Instagram</span></a></li>",
-  "<li><a href='https://github.com/Jacord01' target = 'blank' class='icon style2 fa-github'><span class='label'>GitHub</span></a></li>",
+  "<li><a href='https://github.com/JavierCordero' target = 'blank' class='icon style2 fa-github'><span class='label'>GitHub</span></a></li>",
   "</ul>", "</section>", "<ul class='copyright'>",
   "<li>&copy; Untitled. All rights reserved</li><li>Design: <a href='http://html5up.net'>HTML5 UP</a></li>",
    "</ul>", "</div>", "</footer>"
