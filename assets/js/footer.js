e50ff20ad232d9a4857724e9bbf3166fd470901a
Javier Cordero <ruiderillo@hotmail.es>
2020-02-12 14:49:56 +0100

    Boton de Juvi

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,6 +1,7 @@
 document.write(
   "<footer id='footer'>", "<div class='inner'>", "<section>", "<h2>Follow</h2>", "<ul class='icons'>", 
   "<li><a href='https://twitter.com/Ruiderillo' target = 'blank' class='icon style2 fa-twitter'><span class='label'>Twitter</span></a></li>",
+  "<li><a href='https://twitter.com/IsJuvi' target = 'blank' class='icon style2 fa-twitter'><span class='label'>Twitter</span></a></li>",
   "<li><a href='https://www.facebook.com/javier.corderocalvo' target = 'blank' class='icon style2 fa-facebook'><span class='label'>Facebook</span></a></li>",
   "<li><a href='https://www.instagram.com/ruiderillo/' target = 'blank' class='icon style2 fa-instagram'><span class='label'>Instagram</span></a></li>",
   "<li><a href='https://github.com/JavierCordero' target = 'blank' class='icon style2 fa-github'><span class='label'>GitHub</span></a></li>",
