2aa0e39b4ce5d158692f8dceb0ce37030715eeec
JavierCordero <javiercorderocalvo@gmail.com>
2020-02-18 17:09:50 +0100

    Algun que otro cambio, sobre todo de organizacion

--- assets/js/importHeader.js
+++ assets/js/importHeader.js
@@ -3,8 +3,8 @@ document.write(
 		"<title>Javier Cordero</title>",
 		"<meta charset='utf-8' />",
 		"<meta name='viewport' content='width=device-width, initial-scale=1' />",
-		"<link rel='stylesheet' href='../../assets/css/main.css' />",
-		"<link rel='stylesheet' href='../../assets/css/w3.css'>",
-		"<link rel='stylesheet' href='../../assets/css/languaje.css'/>",
+		"<link rel='stylesheet' href='../../../assets/css/main.css' />",
+		"<link rel='stylesheet' href='../../../assets/css/w3.css'>",
+		"<link rel='stylesheet' href='../../../assets/css/languaje.css'/>",
 		"</head>"
 );
