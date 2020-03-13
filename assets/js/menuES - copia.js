c6698f9b6cb8ef507809ceff2bf5116c6415000d
Javier Cordero <ruiderillo@hotmail.es>
2020-03-13 19:39:54 +0100

    Pequeños cambios

---
+++ assets/js/menuES - copia.js
@@ -0,0 +1,12 @@
+document.write(
+	"<nav id='menu'>",
+	"<h2>Menu</h2>",
+	"<ul>",
+	"<li><a href='indexEN.html'>Main Page</a></li>",
+	"<li><a href='years/primeroES.html'>Videogames</a></li>",
+	"<li><a href='years/primeroES.html'>XR Games</a></li>",
+	"<li><a href='years/segundoES.html'>Mobile Games</a></li>",
+	"<li><a href='years/terceroEN.html'>PS4 Games</a></li>",
+	"</ul>",
+	"</nav>"
+);
--- assets/js/menuES.js
+++ assets/js/menuES.js
@@ -3,9 +3,10 @@ document.write(
 	"<h2>Menu</h2>",
 	"<ul>",
 	"<li><a href='indexES.html'>Página Principal</a></li>",
-	"<li><a href='years/primeroES.html'>Primer cursor</a></li>",
-	"<li><a href='years/segundoES.html'>Segundo Curso</a></li>",
-	"<li><a href='years/terceroEN.html'>Tercer year</a></li>",
+	"<li><a href='videogames/gamesES.html'>Videojuegos</a></li>",
+	"<li><a href='videogames/xrES.html'>Juegos XR</a></li>",
+	"<li><a href='videogames/mobilesES.html'>Juegos para móvil</a></li>",
+	"<li><a href='videogames/ps4ES.html'>Juegos para PS4</a></li>",
 	"</ul>",
 	"</nav>"
 );
