d2a091b6e08f0aabb06a8adbb80ed189b65214f4
Javier Cordero <ruiderillo@hotmail.es>
2020-03-13 14:41:04 +0100

    Cambio de color

---
+++ assets/js/menuES.js
@@ -0,0 +1,11 @@
+document.write(
+	"<nav id='menu'>",
+	"<h2>Menu</h2>",
+	"<ul>",
+	"<li><a href='indexES.html'>Página Principal</a></li>",
+	"<li><a href='years/primeroES.html'>Primer cursor</a></li>",
+	"<li><a href='years/segundoES.html'>Segundo Curso</a></li>",
+	"<li><a href='years/terceroEN.html'>Tercer year</a></li>",
+	"</ul>",
+	"</nav>"
+);
