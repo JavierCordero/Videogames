1af00637f8978eec87e87092b615fd5d58ea0db0
Javier Cordero <ruiderillo@hotmail.es>
2020-03-14 13:21:22 +0100

    Cambios y más cambios

---
+++ assets/js/menuENl1.js
@@ -0,0 +1,12 @@
+document.write(
+	"<nav id='menu'>",
+	"<h2>Menu</h2>",
+	"<ul>",
+	"<li><a href='../indexEN.html'>Main Page</a></li>",
+	"<li><a href='gamesEN.html'>Videogames</a></li>",
+	"<li><a href='xrEN.html'>XR Games</a></li>",
+	"<li><a href='mobilesEN.html'>Mobile Games</a></li>",
+	"<li><a href='ps4EN.html'>PS4 Games</a></li>",
+	"</ul>",
+	"</nav>"
+);
