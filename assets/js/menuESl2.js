e06e09141c6e4f0cb58fe9bfa298eed232157ffc
Javier Cordero <ruiderillo@hotmail.es>
2020-03-15 21:59:31 +0100

    Intento de lfs

---
+++ assets/js/menuESl2.js
@@ -0,0 +1,12 @@
+document.write(
+	"<nav id='menu'>",
+	"<h2>Menu</h2>",
+	"<ul>",
+	"<li><a href='../../../indexES.html'>Página Principal</a></li>",
+	"<li><a href='../../gamesES.html'>Videojuegos</a></li>",
+	"<li><a href='../../xrES.html'>Juegos XR</a></li>",
+	"<li><a href='../../mobilesES.html'>Juegos para móvil</a></li>",
+	"<li><a href='../../ps4ES.html'>Juegos para PS4</a></li>",
+	"</ul>",
+	"</nav>"
+);
